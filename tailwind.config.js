40b56fbc3014be1b0bc6e60787221e52dc3035d5
parsaBordbar <parsab71@gmail.com>
2024-10-23 23:59:06 +0330

    style: fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        im3Red: '#FE3402',
+        im3White: '#F4F4F4',
+        im3Black: '#010101',
+      },
+      fontFamily: {
+        SpaceGrotesk: ['SpaceGrotesk-Regular', 'sans-serif'],
+        Nunito: ['Nunito-Regular', 'sans-serif'],
+      },
+      width: {
+        'fill-available': '-webkit-fill-available',
+      },
+      backgroundImage: {
+        'graph-bg': "url('../public/images/enterRoom/graph.svg')",
+      },
+    },
   },
   plugins: [],
-}
+};
